3209622f371d62aee9991dda0a2ea3fcf7369dcf
Afrin Z <sulthanaafrin94@gmail.com>
2023-09-25 19:43:53 -0500

    new_condition.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,13 @@ let candidateAnswer = ' ';
 
 
 //TODO: Variables for Part 2
-let questions =[];
-let correctAnswers =[];
-let candidateAnswers;
+let questions =["Who was the first American woman in space?", 
+"True or False: 5 kilometer == 5000 meters?", 
+"(5 + 3) / 2 * 10 = ?", 
+"Given the array [8, 'orbit', 'Trajectory', 45], what entry is at index 2?", 
+"What is the minimum crew size for the ISS?"];
+let correctAnswers =["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -23,66 +27,56 @@ candidateName = input.question("Enter Candidate Name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-  candidateAnswer1 = input.question1(question1);
+  for(let i=0; i<5; i++) {
+    //console.log(questions[i]);
+    candidateAnswers[i] = input.questions[i](questions[i]);
+  }
+  //candidateAnswer1 = input.question1(question1);
 }
-let question1 = "Who was the first American woman in space? ";
-let correctAnswer1 = "Sally Ride";
-let candidateAnswer1 = ' ';
-
 
 function gradeQuiz(candidateAnswers1) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if(candidateAnswer1 == correctAnswer1) {
-    console.log("correct answer1");
+  if(candidateAnswer == correctAnswer) {
+    console.log("correct answer");
   } else {
-    console.log("incorrect answer1");
+    console.log("incorrect answer");
   }
-  candidateAnswer2 = input.question2(question2);
 }
-let question2 = "True or False: 5 kilometer == 5000 meters ? ";
-let correctAnswer2 = "true";
-let candidateAnswer2 = ' ';
+
+// function gradeQuiz(candidateAnswers3) {
+
+//   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+//   if(candidateAnswer3 == correctAnswer3) {
+//     console.log("correct answer3");
+//   } else {
+//     console.log("incorrect answer3");
+//   }
+
+//   candidateAnswer3 = input.question3(question3);
+// }
+
+// function gradeQuiz(candidateAnswers3) {
+
+//   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+//   if(candidateAnswer1 == correctAnswer1) {
+//     console.log("correct answer3");
+//   } else {
+//     console.log("incorrect answer3");
+//   }
+
+//   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
-function gradeQuiz(candidateAnswers3) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if(candidateAnswer3 == correctAnswer3) {
-    console.log("correct answer3");
-  } else {
-    console.log("incorrect answer3");
-  }
-
-  candidateAnswer3 = input.question3(question3);
-}
-let question3 = "(5 + 3)/2) * 10 = ?";
-let correctAnswer3 = "40";
-let candidateAnswer3 = ' ';
-
-
-function gradeQuiz(candidateAnswers3) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if(candidateAnswer1 == correctAnswer1) {
-    console.log("correct answer3");
-  } else {
-    console.log("incorrect answer3");
-  }
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
-}
+//   return grade;
+// }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
    console.log("Welcome " +candidateName);
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  //gradeQuiz(this.candidateAnswers);
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
---
+++ studio_string.js
@@ -0,0 +1,2 @@
+let word = 'pseudo-pig latin'
+console.log(word.pop());
